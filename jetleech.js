150bd625e27cfef5dfd0d64864e6837d1ff88403
dvavolta <108687505+dvavolta@users.noreply.github.com>
2022-07-04 19:36:30 +0300

    Create jetleech.js

---
+++ jetleech.js
@@ -0,0 +1,163 @@
+// ==UserScript==
+// @name        jetleech
+// @namespace   Violentmonkey Scripts
+// @match       https://nbyts.online/*
+// @match       https://stfly.me/*
+// @match       https://iir.ai/*
+// @match       https://ouo.io/*
+// @match       https://ouo.press/*
+// @match       https://publicananker.com/*
+// @match       https://informaxonline.com/*
+// @match       https://link1s.com/*
+// @match       https://gaminplay.com/*
+// @match       https://go.adslinkfly.online/*
+// @match       https://tei.ai/*
+// @match       https://makemoneywithurl.com/*
+// @match       https://exey.io/*
+// @match       https://downfile.site/*
+// @match       https://leechall.com/dl/*
+// @match       https://scratch247.info/*
+// @match       https://cocoleech.com/*
+// @match       https://try2link.com/*
+// @match       https://tny.so/*
+// @match       https://fc-lc.com/*
+// @match       https://markipli.com/*
+// @match       https://rodjulian.com/*
+// @match       https://shrinke.me/*
+// @match       https://forex-gold.net/*
+// @match       https://world-trips.net/*
+// @match       https://lucidcam.com/*
+// @match       https://www.restorbio.com/*
+// @grant       none
+// @version     1.0
+// @author      -
+// @description 03.03.2022, 18:17:49
+// ==/UserScript==
+
+
+
+window.addEventListener("load", function() {
+  setTimeout(function() {
+    const ca = document.getElementsByClassName("recaptcha-checkbox-borderAnimation")[0];
+    if (ca) ca.click();
+  },5000);  
+   setTimeout(function() {
+    const ca2 = document.getElementsByClassName("g-recaptcha m-2")[0];
+    if (ca2) ca2.click();
+  },5000);   
+     setTimeout(function() {
+    const ca3 = document.getElementsByClassName("rc-anchor-logo-portrait")[0];
+    if (ca3) ca3.click();
+  },5000);   
+  
+  
+  setInterval(function() {
+    const i = document.querySelector("#surl");
+    if (i) i.click();
+  },5000);
+      setInterval(function() {
+    const k = document.getElementsByClassName("btn btn-success btn-lg get-link")[0];
+    if (k) k.click();
+  },5000);
+        setInterval(function() {
+    const k = document.getElementsByClassName("btn btn-success btn-lg get-link")[0];
+    if (k) k.click();
+  },5000);
+          setInterval(function() {
+    const l = document.querySelector("#btn-main");
+    if (l) l.click();
+  },4000);
+           setInterval(function() {
+    const m = document.getElementsByClassName("g-recaptcha btn btn-primary")[0];
+    if (m) m.click();
+  },4000);
+             setInterval(function() {
+    const n = document.getElementsByClassName("btn btn-primary")[0];
+    if (n) n.click();
+  },4000);
+              setInterval(function() {
+    const o = document.getElementsByClassName("btn btn-success btn-lg get-link")[0];
+    if (o) o.click();
+  },5000);
+                 setInterval(function() {
+    const o = document.getElementsByClassName("btn btn-success btn-lg get-link")[0];
+    if (o) o.click();
+  },5000);
+                  setInterval(function() {
+    const COCO = document.getElementsByClassName("input-group")[0];
+    if (COCO) COCO.click();
+  },5000); 
+  
+  
+                 setInterval(function() {
+    const p = document.querySelector("#scroll > a > p");
+    if (p) p.click();
+  },4000);
+                   setInterval(function() {
+    const r = document.querySelector("#getlink");
+    if (r) r.click();
+  },4000);
+                 setInterval(function() {
+    const s = document.getElementsByClassName("btn btn-primary btn-goo")[0];
+    if (s) s.click();
+  },4000);
+                   setInterval(function() {
+    const s1 = document.getElementsByClassName("btn btn-outline-primary btn-captcha m-2")[0];
+    if (s1) s1.click();
+  },5000);
+                   setInterval(function() {
+    const t = document.getElementsByClassName("btn btn-primary get-link text-white")[0];
+    if (t) t.click();
+  },5000);
+                    setInterval(function() {
+    const tt = document.querySelector("#wpsafe-generate > a > img");
+    if (tt) tt.click();
+  },4000);
+                    setInterval(function() {
+    const ttt = document.querySelector("#wpsafe-link > a > img");
+    if (ttt) ttt.click();
+  },4000);
+                    setInterval(function() {
+    const gg = document.querySelector("#wpsafe-snp > a > img");
+    if (gg) gg.click();
+  },4000);
+                      setInterval(function() {
+    const a1 = document.getElementsByClassName("btn btn-success btn-lg get-link")[0];
+    if (a1) a1.click();
+  },4000);
+                        setInterval(function() {
+    const a2 = document.getElementsByClassName("btn btn-primary")[0];
+    if (a2) a2.click();
+  },7000);
+                          setInterval(function() {
+    const a2a = document.getElementsByClassName("btn btn-primary")[1];
+    if (a2a) a2a.click();
+  },4000);
+                          setInterval(function() {
+    const a3 = document.getElementsByClassName("brand-btn brand-btn-full")[0];
+    if (a3) a3.click();
+  },5000);
+                            setInterval(function() {
+    const a4 = document.getElementsByClassName("btn btn-primary btn-sm btn-captcha")[0];
+    if (a4) a4.click();
+  },5000);
+                            setInterval(function() {
+    const a5 = document.getElementsByClassName("g-recaptcha btn btn-primary")[0];
+    if (a5) a5.click();
+  },5000);
+                              setInterval(function() {
+    const a6 = document.getElementsByClassName("btn m-2 btn-success")[0];
+    if (a6) a6.click();
+  },5000);
+                                setInterval(function() {
+    const a7 = document.getElementsByClassName("s-btn-f")[0];
+    if (a7) a7.click();
+  },5000);
+
+                                  setInterval(function() {
+    const a8 = document.getElementsByClassName("btn btn-success btn-clipboard")[0];
+    if (a78) a8.click();
+  },5000);
+
+
+});
